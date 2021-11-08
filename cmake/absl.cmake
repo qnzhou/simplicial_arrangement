50cb24ec7c0a1dc46cc1abc14054213d49bbee07
Qingnan Zhou <qzhou@adobe.com>
2021-11-08 11:28:46 -0500

    Add github workflow support.

--- cmake/absl.cmake
+++ cmake/absl.cmake
@@ -11,7 +11,9 @@ FetchContent_Declare(
 
 set(OLD_CXX_STANDARD ${CMAKE_CXX_STANDARD})
 set(OLD_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-set(CMAKE_CXX_FLAGS "-Wno-unknown-warning-option")
+if (NOT MSVC)
+    set(CMAKE_CXX_FLAGS "-Wno-unknown-warning-option")
+endif()
 set(CMAKE_CXX_STANDARD 17)
 FetchContent_MakeAvailable(absl)
 set(CMAKE_CXX_FLAGS ${OLD_CXX_FLAGS})
