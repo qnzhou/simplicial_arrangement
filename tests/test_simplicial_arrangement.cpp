00ee943f2c56fce5ac06270df1481824a77facb2
Qingnan Zhou <qzhou@adobe.com>
2022-01-19 10:16:42 -0500

    Windows compilation fix.

--- tests/test_simplicial_arrangement.cpp
+++ tests/test_simplicial_arrangement.cpp
@@ -1,9 +1,12 @@
 #include <simplicial_arrangement/lookup_table.h>
 #include <simplicial_arrangement/simplicial_arrangement.h>
+
+#include <implicit_predicates/implicit_predicates.h>
+
 #include <spdlog/spdlog.h>
 #include <catch2/catch.hpp>
 
-#include <implicit_predicates/implicit_predicates.h>
+#include <cmath>
 
 namespace {
 
