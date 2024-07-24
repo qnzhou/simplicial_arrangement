aa45f5d5ee2ad002d405f897cf6698500cef33fd
Qingnan Zhou <qnzhou@gmail.com>
2024-07-24 18:29:16 -0400

    Update indirect predicates.

--- cmake/indirect_predicates.cmake
+++ cmake/indirect_predicates.cmake
@@ -6,40 +6,35 @@ include(FetchContent)
 FetchContent_Declare(
     indirect_predicates
     GIT_REPOSITORY https://github.com/MarcoAttene/Indirect_Predicates.git
-    GIT_TAG d5966f747cb04bda50243d3273431a7164836085
+    GIT_TAG ca74afb8c664ed24b4482fb3c8a3175102ce3dcb
     )
 
 FetchContent_Populate(indirect_predicates)
 
-SET(INDIRECT_PREDICATES_SRC_FILES
-    ${indirect_predicates_SOURCE_DIR}/numerics.cpp
-    ${indirect_predicates_SOURCE_DIR}/implicit_point.cpp
-    ${indirect_predicates_SOURCE_DIR}/predicates/indirect_predicates.cpp
-    ${indirect_predicates_SOURCE_DIR}/predicates/hand_optimized_predicates.cpp)
 SET(INDIRECT_PREDICATES_INC_FILES
     ${indirect_predicates_SOURCE_DIR}/numerics.h
     ${indirect_predicates_SOURCE_DIR}/implicit_point.h
-    ${indirect_predicates_SOURCE_DIR}/predicates/indirect_predicates.h)
+    ${indirect_predicates_SOURCE_DIR}/indirect_predicates.h)
 
-add_library(indirect_predicates ${INDIRECT_PREDICATES_SRC_FILES} ${INDIRECT_PREDICATES_INC_FILES})
-target_include_directories(indirect_predicates PUBLIC ${indirect_predicates_SOURCE_DIR})
-target_include_directories(indirect_predicates PUBLIC ${indirect_predicates_SOURCE_DIR}/predicates)
-target_compile_features(indirect_predicates PRIVATE cxx_std_11)
+add_library(indirect_predicates INTERFACE)
+#add_library(indirect_predicates ${INDIRECT_PREDICATES_SRC_FILES} ${INDIRECT_PREDICATES_INC_FILES})
+target_include_directories(indirect_predicates INTERFACE ${indirect_predicates_SOURCE_DIR}/include)
+target_compile_features(indirect_predicates INTERFACE cxx_std_20)
 
 # Compiler-specific options
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
     # grant IEEE 754 compliance
-    target_compile_options(indirect_predicates PRIVATE "-frounding-math"
+    target_compile_options(indirect_predicates INTERFACE "-frounding-math"
         "-Wno-format-security")
     # use intrinsic functions (CHECK WHAT TO DO FOR GCC !!!!!!!!)
     # target_compile_options(indirect_predicates PUBLIC "/Oi")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     # grant IEEE 754 compliance
-    target_compile_options(indirect_predicates PRIVATE "/fp:strict")
+    target_compile_options(indirect_predicates INTERFACE "/fp:strict")
     # use intrinsic functions
-    target_compile_options(indirect_predicates PRIVATE "/Oi")
+    target_compile_options(indirect_predicates INTERFACE "/Oi")
     # turn off annoying warnings
-    target_compile_options(indirect_predicates PRIVATE "/D _CRT_SECURE_NO_WARNINGS")
+    target_compile_options(indirect_predicates INTERFACE "/D _CRT_SECURE_NO_WARNINGS")
 elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
     # From the repo README:
     # https://github.com/MarcoAttene/Indirect_Predicates#system-requirements
@@ -47,7 +42,7 @@ elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
     # Mac OSX. Unfortunately CLANG does not support direct access to the
     # floating point environment. The only way to use this software with CLANG
     # is to disable all optimizations (-O0).
-    target_compile_options(indirect_predicates PRIVATE "-O0"
+    target_compile_options(indirect_predicates INTERFACE "-O0"
         "-Wno-format-security" "-Wno-unknown-pragmas" "-Wno-return-type")
 endif()
 
